915fe99f468530f1e058ce7e81169b52f59fa823
Sachin Kumar <sachinthakurela2005@gmail.com>
2026-05-15 16:02:34 +0530

    [Sachin Kumar] add. Added parkinglot-service code

---
+++ parkinglot-service/src/main/java/com/parkease/parkinglot/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.parkease.parkinglot.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
